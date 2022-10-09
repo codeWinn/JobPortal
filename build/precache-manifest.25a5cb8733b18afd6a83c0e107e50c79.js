d8a6026a6b83388783d803a07cdc1f504c1db7fb
codeWinn <adhikarijatin0@gmail.com>
2022-10-09 23:43:50 +0530

    add build

---
+++ build/precache-manifest.25a5cb8733b18afd6a83c0e107e50c79.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5287436e3e904e41f4cc",
+    "url": "/static/css/main.b71d5e53.chunk.css"
+  },
+  {
+    "revision": "5287436e3e904e41f4cc",
+    "url": "/static/js/main.5287436e.chunk.js"
+  },
+  {
+    "revision": "c9fff45489b992630bab",
+    "url": "/static/js/1.c9fff454.chunk.js"
+  },
+  {
+    "revision": "57debad2666a7ca5b1ce",
+    "url": "/static/css/2.77192e01.chunk.css"
+  },
+  {
+    "revision": "57debad2666a7ca5b1ce",
+    "url": "/static/js/2.57debad2.chunk.js"
+  },
+  {
+    "revision": "112cd0797d179d260783",
+    "url": "/static/js/runtime~main.112cd079.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "185bdd3b8cc93546b3a64ce9ea475991",
+    "url": "/static/media/page-title.185bdd3b.jpg"
+  },
+  {
+    "revision": "80b5ef45c3749e1fb2b0ed17ddb27f99",
+    "url": "/index.html"
+  }
+];
